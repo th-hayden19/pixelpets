88f19fbdd0349f1f0c8eb5faa414c40610928160
Thomas Hayden <teh41@pitt.edu>
2025-12-12 16:27:32 -0500

    setup Node.js and wrote pets.ts (UNTESTED)

---
+++ backend/src/pets.ts
@@ -0,0 +1,62 @@
+import express from "express";
+import type {Request, Response, NextFunction} from "express";
+import mqtt from "mqtt";
+
+
+interface PetState {
+    id: string;
+    x: number;
+    y: number;
+    mood: string;
+    hunger: number;
+}
+
+// In-memory dictionary for the states of all pets
+const pets: Record<string, PetState> = {};
+
+// Sets up Express framework which will simplify creating an HTTP server
+const app = express()
+
+const client = mqtt.connect("mqtt://localhost:1883");
+
+// Only when the MQTT client connects, subscribe to (listen for) the given topic pattern (which is the state of each pet ID, shown by wildcard +)
+client.on("connect", () => {
+    client.subscribe("pixelpets/+/state");
+})
+
+// Event listener for MQTT client receiving any message
+client.on("message", (topic, msg) => {
+    try {
+        // Extract ID from string array of pixelpets/{petId}/state
+        const petId = topic.split("/")[1] as string;
+        
+        // Convert msg Buffer payload to a string parsed into JSON
+        const pet: PetState = JSON.parse(msg.toString()); 
+        if (petId !== pet.id) {
+            console.warn(`Warning: pet.id from payload ("${pet.id}") does not match petId from topic ("${petId}")`)
+        }
+
+        // Update the live dictionary with this pet state
+        pets[petId] = pet; 
+
+    } catch (err) {
+        console.error("Failed to process MQTT message: ", err)
+    }
+})
+
+// Middleware (process that happens before sending back response) can log data, errors, etc.
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(err); // Log the error
+    res.status(500).send({ error: 'Something went wrong' }); // Return a user-friendly error
+});
+
+// req is the incoming request object (with query parameters), res is the response object with which we send (JSON) data back to the client (i.e. a browser visiting http://localhost:3000/pets, or 
+// the React dashboard requesting pet states via HTTP)
+app.get("/pets", (req, res) => {
+    res.json(pets)
+})
+
+// Start the server AFTER Express and MQTT clients are fully set up and connected... otherwise MQTT won't be there to handle messages to/from server
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
