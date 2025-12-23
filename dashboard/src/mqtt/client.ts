ccb8c3920e922d0c33d086d77a0c1eaa58f9d035
Thomas Hayden <teh41@pitt.edu>
2025-12-22 20:16:21 -0500

    updated understanding and comments in React client.ts useEffect

--- dashboard/src/mqtt/client.ts
+++ dashboard/src/mqtt/client.ts
@@ -27,11 +27,22 @@ export function usePetMqtt(onPetUpdate: (pet: Pet) => void) {
         return () => {
             client.end();
         };
-    // The arrow function  (prev) => ({ ...prev, [updatedPet.id]: updatedPet }) is not stable, so every time handlePetUpdate is called, a new function OBJECT is created for prev => ...
-    // Similarly, the pet argument changes every time a new Pet is passed in
-    // Therefore we need to pass in this external value? Not very clear...
+
+    // 1. An effect runs whenever a variable in its dependency array changes value.
+    //    The dependency array must contain any external values that are read inside the effect, 
+    //    so that the effect can re-run and stay in sync with changes to those values.
+    //    (Guarunteed stable external values, such as setState functions, are exceptions and do not need to be included.)
+    // 2. Whenever a function is rendered, a new function object and reference are created in memory,
+    //    even if the function object is identical to the previous one.
+    //    A variable holding the reference now updates its value to the newly made reference.
+    //
+    // Implementation:
+    // 1. Normally, [onPetUpdate] would cause this effect to run on every render in which the onPetUpdate function reference changes, 
+    //    forcing unecessary mqtt client object connections.
+    // 2. To prevent this, we memoize the passed-in function from App.tsx with useCallback(), 
+    //    which "freezes the reference" (new references/objects are not created until dependencies change;
+    //    in our case, the useCallback dependency array is [], so a new reference/object will never be made).
+    // 3. The result is a proper dependency array with the external value onPetUpdate, 
+    //    while ensuring this useEffect only ever runs once, on mount.
     }, [onPetUpdate]);
 }
-// The aha - moment is to stop thinking about use effect as "run this code when the component mounts" or "run this code when this value changes".
-// Instead it should be seen as a way of synchronizing your React components state with some external API.
-// When you look at it like that it becomes clear that you need to re-evaluate the synchronization when any dependency changes.
